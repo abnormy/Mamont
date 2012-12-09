2042162dd350d805f6afee75cadca0b192c42f03
corwin <i.kesler@live.com>
2012-12-09 12:13:01 +0200

    Joining TDD

---
+++ UnitTestss/ControllerTests/UserControllerTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace UnitTestss.ControllerTests
+{
+    [TestFixture]
+    public class UserControllerTests
+    {
+        public void TheTest()
+        {
+             
+        }
+    }
+}
